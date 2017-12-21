30881642c99f16225480f0df20c30ae945785981
yxnne <506524339@qq.com>
2017-12-22 00:17:20 +0800

    增加了antd的修改样式的配置 基本就是： 1.给babel-loader装插件{libraryName: "antd",style: true}; 2.新建一个less文件配置样式并引入到webpack.config.jsz中。 3.配置各种less-loader style-loader css-loader等。然后用之前引入的样式，通过less-loader覆盖原有样式。

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,61 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const fs = require('fs');
+
+const lessToJs = require('less-vars-to-js');
+const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, './theme.less'), 'utf8'));
 module.exports = {
-  context : __dirname + '/src',
-  entry   : "./js/root.js",
-  module  : {
-    loaders   :[{
-      test : /.js?$/,
-      exclude:/(node_modules)/,
-      loader: 'babel-loader',
-      query :{
-        presets:['react' , 'babel-preset-react']
-      }
-    },
-    {
-      //css模块化设置
-      test : /\.css$/,
-      //ant-design样式需要这样
-      loader : 'style-loader!css-loader'
-    },
+  context: __dirname + '/src',
+  entry: "./js/root.js",
+  module: {
+
+    loaders: [{
+        test: /.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'babel-preset-react'],
+          plugins: [
+            ['import', {
+              libraryName: "antd",
+              style: true
+            }]
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+
+      {
+        //css模块化设置
+        test: /\.less$/,
+        //ant-design样式需要这样
+        use: [{
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "less-loader",
+            options: {
+              modifyVars: themeVariables
+            }
+          }
+        ]
+      },
     ]
   },
-  output:{
-    path:__dirname + "/src/",
-    filename:"bundle.js"
+  output: {
+    path: __dirname + "/src/",
+    filename: "bundle.js"
   },
-  devServer:{
+  devServer: {
     //我们在这里对webpack-dev-server进行配置
-    port:18080,
-    historyApiFallback:true
+    port: 18080,
+    historyApiFallback: true
   }
 };
