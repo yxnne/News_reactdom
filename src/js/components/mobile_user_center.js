bf7f12ac3c2ad7518f91f5d6f5899f03b445d037
iel-mac <506524339@qq.com>
2018-01-19 09:54:11 +0800

    用户中心简单添加功能：查看评论和查看收藏和上传头像（只是测试了下upload）

--- src/js/components/mobile_user_center.js
+++ src/js/components/mobile_user_center.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, Tabs} from 'antd';
+import {Row, Col, Tabs, Upload, Icon, Modal, Card} from 'antd';
 
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
@@ -7,8 +7,64 @@ import MobileFooter from './mobile_footer';
 const TabPane = Tabs.TabPane;
 
 export default class MobileUserCenter extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      usercollection: '',
+      usercomments: '',
+      previewImage: '',
+      previewVisible: false
+    };
+  }
+
+  componentDidMount(){
+    var myFetchOptions = {
+      method: 'GET'
+    };
+
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=" +
+    localStorage.userid, myFetchOptions)
+    .then(response=>response.json())
+    .then(json=>{
+      this.setState({usercollection:json});
+    });
+
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=" +
+    localStorage.userid, myFetchOptions)
+    .then(response=>response.json())
+    .then(json=>{
+      this.setState({usercomments:json});
+    });
+  }
 
   render(){
+
+    //收藏
+    const {usercollection, usercomments} = this.state;
+    const usercollectionList = usercollection.length ?
+    usercollection.map((uc, index)=>(
+      <Card key={index} title={uc.uniquekey}  hoverable
+      extra={<a href={`/details/${uc.uniquekey}`}>查看</a>}>
+        {/*/#/details/${uc.uniquekey} 这是a 标签href的写法*/}
+        <p>{uc.Title}</p>
+      </Card>
+    ))
+    :
+    "there are no news you collected"
+    ;
+
+    //评论
+    const usercommentsList = usercomments.length ?
+    usercomments.map((comment,index)=>(
+        <Card key={index} title={`于 ${comment.datetime} 评论了文章`}
+        hoverable extra={<a href={`/details/${comment.uniquekey}`}>查看</a>}>
+          <p>{comment.Comments}</p>
+        </Card>
+    ))
+    :
+    'No Comments'
+    ;
+
     return (
 
       <div>
@@ -17,10 +73,20 @@ export default class MobileUserCenter extends React.Component{
           <Col span={2}/>
           <Col span={20}>
             <Tabs>
-              <TabPane key={1} tab="My Restore">
+              <TabPane key={1} tab="My Colection">
+                <Row>
+                  <Col span={24}>
+                    {usercollectionList}
+                  </Col>
+                </Row>
               </TabPane>
 
               <TabPane key={2} tab="My Comments">
+                <Row>
+                  <Col span={24}>
+                    {usercommentsList}
+                  </Col>
+                </Row>
               </TabPane>
 
               <TabPane key={3} tab="My Portrait">
--- src/js/components/pc_user_center.js
+++ src/js/components/pc_user_center.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, Tabs} from 'antd';
+import {Row, Col, Tabs, Upload, Icon, Modal, Card} from 'antd';
 
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
@@ -7,26 +7,132 @@ import PCFooter from './pc_footer';
 const TabPane = Tabs.TabPane;
 
 export default class PCUserCenter extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      usercollection: '',
+      usercomments: '',
+      previewImage: '',
+      previewVisible: false
+    };
+  }
+
+  componentDidMount(){
+    var myFetchOptions = {
+      method: 'GET'
+    };
+
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=" +
+    localStorage.userid, myFetchOptions)
+    .then(response=>response.json())
+    .then(json=>{
+      this.setState({usercollection:json});
+    });
+
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=" +
+    localStorage.userid, myFetchOptions)
+    .then(response=>response.json())
+    .then(json=>{
+      this.setState({usercomments:json});
+    });
+  }
 
   render(){
+    //上传的属性
+    const props={
+			action: 'http://newsapi.gugujiankong.com/handler.ashx',
+      //头部跨域
+			headers: {
+				"Access-Control-Allow-Origin": "*"
+			},
+			listType: 'picture-card',
+      // 指定的默认的东西
+      defaultFileList: [
+				{
+					uid: -1,
+					name: 'xxx.png',
+					state: 'done',
+					url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+					thumbUrl: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',  // 缩略图url
+				}
+			],
+      // 预览的回调
+			onPreview: (file) => {
+				this.setState({previewImage: file.url, previewVisible: true});
+			}
+    };
+
+    //收藏
+    const {usercollection} = this.state;
+    const usercollectionList = usercollection.length ?
+    usercollection.map((uc, index)=>(
+      <Card key={index} title={uc.uniquekey} hoverable
+      extra={<a target="_blank" href={`/details/${uc.uniquekey}`}>查看</a>}>
+        {/*/details/${uc.uniquekey} 这是a 标签href的写法*/}
+				<p>{uc.Title}</p>
+			</Card>
+    ))
+    :
+    "there are no news you collected"
+    ;
+
+    //评论
+    const usercommentsList = usercomments.length ?
+    usercomments.map((comment,index)=>(
+        <Card key={index} title={`于 ${comment.datetime} 评论了文章`} hoverable
+        extra={<a href={`/details/${comment.uniquekey}`}>查看</a>}>
+          <p>{comment.Comments}</p>
+        </Card>
+    ))
+    :
+    'No Comments';
+
     return (
 
       <div>
         <PCHeader />
         <Row>
           <Col span={2}/>
+
           <Col span={20}>
             <Tabs>
-              <TabPane key={1} tab="My Restore">
+              <TabPane key={1} tab="My Collection">
+                <div className="comment">
+                  <Row>
+                    <Col span={24}>
+                      {usercollectionList}
+                    </Col>
+                  </Row>
+                </div>
               </TabPane>
 
               <TabPane key={2} tab="My Comments">
+                <div class="comment">
+  								<Row>
+  									<Col span={24}>
+  										{usercommentsList}
+  									</Col>
+  								</Row>
+  							</div>
               </TabPane>
 
               <TabPane key={3} tab="My Portrait">
+                <div className="clearfix">
+                  <Upload {...props}>
+                    <Icon type="plus"/>
+                      <div className="ant-upload-text">
+                        Upload Portrait
+                      </div>
+                      <Modal visible={this.state.previewVisible} footer={null}
+                      onCancel={this.handleCancel}>
+                        <img  ale="View" src={this.state.previewImage}/>
+                      </Modal>
+                  </Upload>
+                </div>
               </TabPane>
             </Tabs>
           </Col>
+
           <Col span={2}/>
 
         </Row>
